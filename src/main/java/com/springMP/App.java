282a71eca0398694a27321bc5a12b48c32c5e2c0
C <viranidhruv002@gmail.com>
2023-05-10 18:51:23 +0530

    Done

---
+++ src/main/java/com/springMP/App.java
@@ -0,0 +1,17 @@
+package com.springMP;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        ApplicationContext con = new ClassPathXmlApplicationContext("config.xml");
+        Student s1 = (Student) con.getBean("s1");
+        System.out.println(s1);
+        
+        Student s2 = (Student) con.getBean("s2");
+        System.out.println(s2);
+    }
+}
